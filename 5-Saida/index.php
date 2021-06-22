e6cc5c1d7138a752a398db24f289593570b88128
JalesMonteiro <>
2021-06-22 06:22:18 +0000

    saidas

---
+++ 5-Saida/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Comentários</title>
+  </head>
+  <body>
+    <?php
+      echo '<p>Oi Gily!</p>';
+
+      print('<b>Oi Islânio!</b><br/>');
+
+      $vetor = [1,2,3,4,5];
+
+      var_dump($vetor);
+      echo '<br/>';
+      print_r($vetor);
+    ?> 
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <a href="1-Oi-mundo/index.php">1 Oi mundo!</a><br/>
+    <a href="2-Variaveis-e-Constantes/index.php">2 Variáveis e constantes</a><br/>
+    <a href="3-Operadores/index.php">3 Operadores</a><br/>
+    <a href="4-Comentarios/index.php">4 Comentários</a><br/>
+    <a href="5-Saida/index.php">5 Saida</a><br/>
+  </body>
+</html>
